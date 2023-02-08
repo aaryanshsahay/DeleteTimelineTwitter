2a6194dab43791b6e27ced710314388227f6bb3b
Aaryansh Sahay <53135035+aaryanshsahay@users.noreply.github.com>
2023-02-08 19:26:16 +0530

    Update main.py
    added comments & print statements for better visibility

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+"""
+============================================================================
+
+
+	100% Free of cost.
+
+	Go thorugh the read me file first!!!
+	There's a guide both on how to use this for both, people who
+	dont know how to code as well as people who do know.
+
+	Enjoy! 
+
+    @AaryanshSahay February 2023.
+	
+
+============================================================================
+"""
+
+
 import yaml
 from yaml.loader import SafeLoader
 import tweepy
@@ -10,6 +29,18 @@ import argparse
 
 
 def initializeEnvironment(file_path):
+	"""
+	Reads the appropriate api keys and access tokens from the config.yaml file,
+	initialzies the environment and sets the 'api' variable which will be used 
+	later.
+
+	Args :
+		1) file_path (str) : relative path to the config.yaml file
+
+	Returns : 
+		1) tweepy api object
+
+	"""
 	try:
 		with open(file_path) as f:
 			data = yaml.load(f, Loader = SafeLoader)
@@ -27,6 +58,17 @@ def initializeEnvironment(file_path):
 
 
 def DeleteSingleTweet(status_id):
+	"""
+	Deletes a single tweet permanently. Does not return anything.
+	api is not passed as an argument because this function will inherit from a parent function
+	which already has the api argument initialized.
+
+	Args : 
+		1) status_id (str) : id of a tweet
+
+	returns :
+		None
+	"""
 	try:
 		api.destroy_status(status_id)
 	except:
@@ -34,8 +76,18 @@ def DeleteSingleTweet(status_id):
 
 
 def DeleteTweetsCount(api, count = None):
+	"""
+	Deletes last 'n' tweets from the most recent tweet.
+
+	Args : 
+		1) api : tweepy api object
+		2) count (int) : number of tweets to be deleted. Defaults to None.
+
+	returns :
+		None
+	"""
 	try:	
-	
+		print("="*25,"Deleting Tweets!!!!","="*25)
 		num = 0
 		for tweet in tweepy.Cursor(api.user_timeline).items():
 				DeleteSingleTweet(status_id = tweet.id)
@@ -50,8 +102,18 @@ def DeleteTweetsCount(api, count = None):
 
 
 def DeleteTweetsSince(api, days = None):
+	"""
+	Deletes tweets from the last 'n' days from the day of running the script.
+
+	Args :
+		1) api : tweepy api object
+		2) days (int) : the number of days, defaults to None.
+	
+	returns :
+		None
+	"""  
 	try:
-		
+		print("="*25,"Deleting Tweets!!!!","="*25)
 		count = 0
 		for tweet in tweepy.Cursor(api.user_timeline, since_id = str(datetime.date.today() - timedelta(days = days))).items():
 			DeleteSingleTweet(status_id = tweet.id)
@@ -64,7 +126,17 @@ def DeleteTweetsSince(api, days = None):
 
 
 def DeleteAllTweets(api):
+	"""
+	Deletes every single tweet in the user's timeline. The max limit is 3000. (tweepy limitation)
+	
+	Args : 
+		1) api : tweepy api object
+
+	returns : 
+		None
+	"""
 	try:
+		print("="*25,"Deleting Tweets!!!!","="*25)
 		count = 0
 		for tweet in tweepy.Cursor(api.user_timeline).items():
 			DeleteSingleTweet(status_id = tweet.id)
